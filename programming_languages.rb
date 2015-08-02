1d2f5fc505426a8565d7ec0df7f351d2dce0fab9
Tyler Machen <tylermachen@gmail.com>
2015-08-01 23:58:53 -0400

    complete programming languages methods

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
+def languages
+  languages = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+
+    }
+  }
+end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      new_hash[language] = type; new_hash[language][:style] = [style]
+      type[:style] << :oo if style == :functional && language == :javascript
+    end
+  end
+  new_hash
 end
